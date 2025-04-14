3e972463c83bc4ad66e303cf620e46f46c50fdc7
Simon Coles <simonc@amphora-research.com>
2025-04-14 10:11:02 +0100

    Use the database for sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_13_154245) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_14_090939) do
   create_table "encryption_keys", force: :cascade do |t|
     t.text "public_key"
     t.text "private_key"
@@ -24,5 +24,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_13_154245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category", default: "Diary", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 end
