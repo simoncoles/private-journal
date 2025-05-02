3f7d4020a490727c17ca6fc092de3622fb3422ff
Simon Coles <simonc@amphora-research.com>
2025-05-02 11:54:39 +0100

    Remove content from logging

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,9 @@
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
+  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc,
+  # Filter sensitive content from entries
+  "entry.content", 
+  # Filter attachment data
+  "attachment.data"
 ]
